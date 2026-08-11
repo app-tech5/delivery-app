72e1b8d98c2557866625d2870f7f4600a75f5b3a
nass <cabliveer@gmail.com>
2026-08-11 09:25:47 +0100

    Use official Ionicons restaurant-outline SVG for web map markers.

--- components/map/DriverNearbyMap.web.js
+++ components/map/DriverNearbyMap.web.js
@@ -67,14 +67,12 @@ const buildMapHtml = (center, markers) => `<!DOCTYPE html>
         '</svg></div></div>';
     }
 
-    // Ionicons restaurant-outline (same family as MapEntityMarker)
+    // Exact Ionicons restaurant-outline (same asset as MapEntityMarker / @expo/vector-icons)
     function restaurantHtml() {
-      return '<div class="marker-root"><div class="icon-ring" style="border-color:' + RESTAURANT_COLOR + '">' +
+      return '<div class="marker-root"><div class="icon-ring">' +
         '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 512 512">' +
-        '<path fill="none" stroke="' + RESTAURANT_COLOR + '" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M80 144v112a80 80 0 00160 0V144"/>' +
-        '<path fill="none" stroke="' + RESTAURANT_COLOR + '" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M80 80v32m40-32v32m40-32v32m40-32v32"/>' +
-        '<path fill="none" stroke="' + RESTAURANT_COLOR + '" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M160 256v176"/>' +
-        '<path fill="none" stroke="' + RESTAURANT_COLOR + '" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M336 176v176m48-240c0 80-48 96-48 176v144m48-240v-16a48 48 0 00-96 0v16"/>' +
+        '<path d="M57.49 47.74l368.43 368.43a37.28 37.28 0 010 52.72h0a37.29 37.29 0 01-52.72 0l-90-91.55a32 32 0 01-9.2-22.43v-5.53a32 32 0 00-9.52-22.78l-11.62-10.73a32 32 0 00-29.8-7.44h0a48.53 48.53 0 01-46.56-12.63l-85.43-85.44C40.39 159.68 21.74 83.15 57.49 47.74z" fill="none" stroke="' + RESTAURANT_COLOR + '" stroke-linejoin="round" stroke-width="32"/>' +
+        '<path d="M400 32l-77.25 77.25A64 64 0 00304 154.51v14.86a16 16 0 01-4.69 11.32L288 192M320 224l11.31-11.31a16 16 0 0111.32-4.69h14.86a64 64 0 0045.26-18.75L480 112M440 72l-80 80M200 368l-99.72 100.28a40 40 0 01-56.56 0h0a40 40 0 010-56.56L128 328" fill="none" stroke="' + RESTAURANT_COLOR + '" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>' +
         '</svg></div></div>';
     }
 
